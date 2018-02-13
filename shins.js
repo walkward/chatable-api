d74739c816caf74d72b33cdb381bc42c3de82f94
Walker Ward <walker.ward1@gmail.com>
2018-02-12 17:04:18 -0700

    updates

--- shins.js
+++ shins.js
@@ -2,12 +2,12 @@ var shins = require('shins');
 var fs = require('fs')
 var markdown = fs.readFileSync(__dirname + '/docs/markdown.md','utf8')
 var options = {};
-options.minify = false;
+options.minify = true;
 options.customCss = false;
-options.inline = false;
+options.inline = true;
 options.unsafe = false; // setting to true turns off markdown sanitisation
 options.source = '/docs/markdown.md'; // used to resolve relative paths for included files
-console.log(markdown)
+
 shins.render(markdown, options, function(err, html) {
   let writeStream = fs.createWriteStream('docs/index.html')
   writeStream.write(html, 'utf8');
