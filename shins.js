ba85b36c1dbabaf3311822e4831aa97a3a8832ab
Walker Ward <walker.ward1@gmail.com>
2018-02-13 18:48:40 -0700

    updates

--- shins.js
+++ shins.js
@@ -2,6 +2,7 @@ var shins = require('shins');
 var fs = require('fs')
 var markdown = fs.readFileSync(__dirname + '/docs/markdown.md','utf8')
 var options = {};
+options.highlighter_theme = 'Atom One Dark'
 options.minify = true;
 options.customCss = false;
 options.inline = true;
@@ -11,5 +12,6 @@ options.source = '/docs/markdown.md'; // used to resolve relative paths for incl
 shins.render(markdown, options, function(err, html) {
   let writeStream = fs.createWriteStream('docs/index.html')
   writeStream.write(html, 'utf8');
-  console.log(err)
+  // Log errors if they exist.
+  if (err) console.log(err)
 });
